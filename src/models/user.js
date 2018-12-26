f354c42e18564f2399c519a1d1d6f363d6a86b07
Avinash <avinashb97@gmail.com>
2018-12-27 00:49:17 +0530

    Adds User model

---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+UserSchema.post('save', () => {
+  const data = this;
+  data.lastUpdateAt = new Date();
+});
+
+module.exports = mongoose.model('Message', UserSchema);
