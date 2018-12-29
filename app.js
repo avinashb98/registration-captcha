a8ae6f7adeb5709fa1a930f799a382be165c9880
Avinash <avinashb97@gmail.com>
2018-12-29 15:13:13 +0530

    Adds Cors

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const registerRoute = require('./src/routes/register');
+
 // mongodb config
 require('./config/db');
 
 // Initializing express app
 const app = express();
+
+
+app.use(cors());
 
 // Body Parser Configuration
 app.use(bodyParser.json({ // to support JSON-encoded bodies
