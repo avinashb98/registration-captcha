11b8e673f1c525be56685c153433d531197b78bb
Avinash <avinashb97@gmail.com>
2018-12-29 18:25:02 +0530

    Change Base url to live

--- client/register-captcha/src/app/register.service.ts
+++ client/register-captcha/src/app/register.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RegisterService {
 
-  private baseUrl = 'http://localhost:3000/api';
+  private baseUrl = 'http://calm-refuge-89560.herokuapp.com/api';
 
   constructor(private http: HttpClient) { }
 
